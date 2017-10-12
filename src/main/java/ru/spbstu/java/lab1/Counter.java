2459be12cbf76dcea53eb9c9de4b47d024aed8b3
Evgenii Nemich <evgenii_nemich@epam.com>
2017-10-13 01:33:05 +0300

    lab1

---
+++ src/main/java/ru/spbstu/java/lab1/Counter.java
@@ -0,0 +1,26 @@
+package ru.spbstu.java.lab1;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import java.util.function.Function;
+
+/**
+ * Allowed operations: -+/*()
+ */
+public class Counter implements Function<String, Double> {
+    @Override
+    public Double apply(String s) {
+        Double result = null;
+        ScriptEngineManager mgr = new ScriptEngineManager();
+        ScriptEngine engine = mgr.getEngineByName("JavaScript");
+        try {
+            result = Double.valueOf(engine.eval(s).toString());
+            System.out.println(s + " = " + result + " is " + engine.eval(s).toString());
+        } catch (ScriptException e) {
+            System.out.println("Wrong Expression");
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
